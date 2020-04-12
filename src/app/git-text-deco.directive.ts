dca5fe9c49888388d9e655fe02f24efdc008e732
Maureen <murimimaureen69@gmail.com>
2020-04-12 23:51:01 +0300

    add a jumbotron to navbar

--- src/app/git-text-deco.directive.ts
+++ src/app/git-text-deco.directive.ts
@@ -1,4 +1,4 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appGitTextDeco]'
@@ -6,5 +6,16 @@ import { Directive } from '@angular/core';
 export class GitTextDecoDirective {
 
   constructor() { }
+
+  textColor = ['lavender', 'darkblue'];
+
+  @HostBinding('style.color') color: string;
+  @HostBinding('style.border-color') borderColor: string;
+
+  @HostListener('keydown') newColor() {
+    const colorPick = Math.floor(Math.random() * this.textColor.length);
+
+    this.color = this.borderColor = this.textColor[colorPick];
+  }
 
 }
