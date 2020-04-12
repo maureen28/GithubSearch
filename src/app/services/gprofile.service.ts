f33d2e6e428995601a0103b89460e979418db115
Maureen <murimimaureen69@gmail.com>
2020-04-13 00:14:13 +0300

    add clientId and clientSecret

--- src/app/services/gprofile.service.ts
+++ src/app/services/gprofile.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GprofileService {
+  private username: string;
+  private clientId: '0ae4c8853c92e70867a5';
+  private clientSecret: '7716f03d7070910448a899b72d01336d866a86e1';
 
-  constructor() { }
+  constructor() {}
 }
