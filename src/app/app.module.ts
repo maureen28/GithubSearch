95521ab47f19215ef800f36ff8b8e695630ebc7a
Maureen <murimimaureen69@gmail.com>
2020-04-13 00:00:33 +0300

    add a form to profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,26 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { DatePassedPipe } from './date-passed.pipe';
 import { GitTextDecoDirective } from './git-text-deco.directive';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { RepoComponent } from './components/repo/repo.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     DatePassedPipe,
-    GitTextDecoDirective
+    GitTextDecoDirective,
+    NotFoundComponent,
+    ProfileComponent,
+    RepoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
