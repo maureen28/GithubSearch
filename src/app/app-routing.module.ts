f1d59c1cbc5cbef3e5f2b4ebf4fd9f0d3261a751
Maureen <murimimaureen69@gmail.com>
2020-04-14 15:45:52 +0300

    Add a profile and user service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
+import { UsersComponent } from './components/users/users.component';
+import { ProfileComponent } from './components/profile/profile.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
+
 const routes: Routes = [
+  { path: 'profile', component: ProfileComponent },
+  { path: 'users', component: UsersComponent },
+  {path: 'repo' , component: },
+  { path: '', redirectTo: '/users', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
