e3c670d0f2a8791af681a340ea5ca79c4e25a5b5
Maureen <murimimaureen69@gmail.com>
2020-04-14 12:32:22 +0300

    Delete all components and start a new

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,9 @@
 import { NotFoundComponent } from './components/not-found/not-found.component';
-import { ProfileDetailsComponent } from './components/profile-details/profile-details.component';
-import { UserComponent } from './components/user/user.component';
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  {path: 'profile-details', component: ProfileDetailsComponent},
-  {path: 'user', component: UserComponent},
-  { path: '', redirectTo: '/users', pathMatch: 'full' },
-  {path: '**', component: NotFoundComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { DatePassedPipe } from './date-passed.pipe';
 import { GitTextDecoDirective } from './git-text-deco.directive';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { ProfileDetailsComponent } from './components/profile-details/profile-details.component';
-import { UserComponent } from './components/user/user.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
+
     DatePassedPipe,
     GitTextDecoDirective,
     NotFoundComponent,
-    ProfileDetailsComponent,
-    UserComponent,
+
   ],
   imports: [
     BrowserModule,
